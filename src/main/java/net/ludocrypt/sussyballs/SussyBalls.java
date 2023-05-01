4ae9fd2c00656a135c776696768f845e2bbe0aaa
LudoCrypt <60561627+LudoCrypt@users.noreply.github.com>
2023-05-01 14:18:57 -0500

    Updated Dispensers

--- src/main/java/net/ludocrypt/sussyballs/SussyBalls.java
+++ src/main/java/net/ludocrypt/sussyballs/SussyBalls.java
@@ -1,11 +1,19 @@
 package net.ludocrypt.sussyballs;
 
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.block.DispenserBlock;
+import net.minecraft.block.dispenser.ProjectileDispenserBehavior;
+import net.minecraft.entity.projectile.ProjectileEntity;
+import net.minecraft.entity.projectile.thrown.SnowballEntity;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraft.recipe.RecipeSerializer;
 import net.minecraft.recipe.SpecialRecipeSerializer;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.Util;
+import net.minecraft.util.math.Position;
 import net.minecraft.util.registry.Registry;
+import net.minecraft.world.World;
 
 public class SussyBalls implements ModInitializer {
 
@@ -16,6 +24,13 @@ public class SussyBalls implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
+		DispenserBlock.registerBehavior(SUSPICIOUS_SNOWBALL, new ProjectileDispenserBehavior() {
 
+			@Override
+			protected ProjectileEntity createProjectile(World world, Position position, ItemStack stack) {
+				return Util.make(new SnowballEntity(world, position.getX(), position.getY(), position.getZ()), entity -> entity.setItem(stack));
+			}
+
+		});
 	}
 }
